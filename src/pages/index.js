01ebf80f4fc15419511ef66a013c37cfae51826f
Sara <sala0002@stud.kea.dk>
2025-05-06 14:37:44 +0200

    yes

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,13 +6,13 @@ import Footer from "@/components/Footer";
 export default function Home() {
   return (
     <>
-    <div className="lg:ml-[60px] lg:mr-[240px]">
-    <HeroSlide/>
-    <h2 className="pt-10 pb-10 ">Nyeste nyheder</h2>
-      
-<Cards/>
-<DiagonalStripe color="#FFE3F5" height="350px" />
-</div>  
+      <div className="lg:ml-[60px] lg:mr-[240px]">
+        <HeroSlide />
+        <h2 className="pt-10 pb-10 ">Nyeste nyheder</h2>
+
+        <Cards />
+        <DiagonalStripe color="#FFE3F5" height="350px" />
+      </div>
       <div className="ml-[60px mr-[240px]">
         <HeroSlide />
         <DiagonalStripe color="#FFE3F5" height="350px" />
