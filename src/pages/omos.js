666cf516134395aa70d793c6ec947032659dce8b
Maria <madr0002@stud.kea.dk>
2025-05-16 10:44:12 +0200

    yes

--- src/pages/omos.js
+++ src/pages/omos.js
@@ -14,7 +14,7 @@ export default function OmOs() {
         <title>Om os – Fashion Forum</title>
         <meta name="description" content="Læs om hvem vi er hos Fashion Forum – Danmarks førende digitale medie og jobportal for modebranchen." />
       </Head>
-      <div className=" lg:ml-[60px] lg:mr-[240px]"><Redaktion />
+      <div className=" lg:ml-[60px] lg:mr-[230px]"><Redaktion />
       <Quote />
       <AboutText />
       <News />
